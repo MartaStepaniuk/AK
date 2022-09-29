4388889d55dd0b38e6cf92c6f32ee1baa443dbb9
Marta Stepaniuk <martastepaniuk52@gmail.com>
2022-09-29 22:13:33 +0300

    changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,18 @@
+//some changes
 #include "calculator.h"
-
+//operation Add
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
-
+//operation Sub
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
-
+//operation Mul
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+//some changes
